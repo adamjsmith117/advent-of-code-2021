a1af586f80de9922d731ac2313afd2aa752792a7
Adam Smith <adamjsmith117@gmail.com>
2021-12-04 23:59:19 -0700

    started day4

--- src/day4/part1.ts
+++ src/day4/part1.ts
@@ -1,7 +1,28 @@
 import { readFile, reportAnswer } from "../utils";
 
+type BoardCollection = {
+  boards: Board[];
+  numbers: number[];
+};
+
+type Board = {
+  board: number[][]; // 5x5 set of numbers,
+  markedIndices: { x: number; y: number }[];
+};
+
+// const buildBoard = (boardData: string[]): Board => {};
+
+const buildBoardCollection = (filepath: string): BoardCollection => {
+  const lines = readFile(filepath);
+  const collection: BoardCollection = {
+    numbers: lines[0].split(",").map(Number),
+    boards: [], // TODO
+  };
+  return collection;
+};
+
 const solve = (filepath: string): number => {
-  const input = readFile(filepath);
+  const collection = buildBoardCollection(filepath);
   return 0;
 };
 
