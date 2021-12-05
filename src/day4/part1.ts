80d7acd4cad696ec014796d6d1db3e38053f029b
Adam Smith <adamjsmith117@gmail.com>
2021-12-04 23:46:20 -0700

    set up files for day4

---
+++ src/day4/part1.ts
@@ -0,0 +1,14 @@
+import { readFile, reportAnswer } from "../utils";
+
+const solve = (filepath: string): number => {
+  const input = readFile(filepath);
+  return 0;
+};
+
+export const runTest = (): number => solve("data/day4/test-data.txt");
+
+export const run = (): number => {
+  const answer = solve("data/day4/data.txt");
+  reportAnswer(4, 1, answer);
+  return answer;
+};
