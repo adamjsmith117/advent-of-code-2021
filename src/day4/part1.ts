0e4d41d97f75816417d371d980f3b1f1d52d857d
adamjsmith117 <adamjsmith117@gmail.com>
2021-12-06 22:23:59 -0700

    start of part2

--- src/day4/part1.ts
+++ src/day4/part1.ts
@@ -1,30 +1,155 @@
 import { readFile, reportAnswer } from "../utils";
 
+const BOARD_DIMENSION = 5;
+
 type BoardCollection = {
   boards: Board[];
-  numbers: number[];
+  numberCage: number[];
 };
+
+type Coord = { x: number; y: number; value: number };
 
 type Board = {
-  board: number[][]; // 5x5 set of numbers,
-  markedIndices: { x: number; y: number }[];
+  board: number[][];
+  markedCoords: Coord[];
 };
 
-// const buildBoard = (boardData: string[]): Board => {};
+const runGame = (collection: BoardCollection): number => {
+  let round = 0;
+  let winningBoard = {} as Board;
+  let winnerFound = false;
+  let mostRecentlyPulledNumber;
+  while (!winnerFound) {
+    mostRecentlyPulledNumber = processRound(collection, round);
+    collection.boards.forEach((board: Board) => {
+      if (isWinner(board)) {
+        winningBoard = board;
+        winnerFound = true;
+        return;
+      }
+    });
+    round++;
+  }
+  return calculateScore(winningBoard, mostRecentlyPulledNumber as number);
+};
+
+const calculateScore = (
+  board: Board,
+  mostRecentlyPulledNumber: number
+): number => {
+  let sum = 0;
+  for (let i = 0; i < BOARD_DIMENSION; i++) {
+    for (let j = 0; j < BOARD_DIMENSION; j++) {
+      if (
+        !board.markedCoords.some(
+          (coord: Coord) => coord.x === i && coord.y === j
+        )
+      ) {
+        sum += board.board[i][j];
+      }
+    }
+  }
+  return sum * mostRecentlyPulledNumber;
+};
+
+const processRound = (collection: BoardCollection, round: number): number => {
+  const drawnNumber = collection.numberCage[round];
+  collection.boards.forEach((board: Board) => {
+    for (let i = 0; i < BOARD_DIMENSION; i++) {
+      for (let j = 0; j < BOARD_DIMENSION; j++) {
+        if (board.board[i][j] === drawnNumber) {
+          board.markedCoords.push({ x: i, y: j, value: board.board[i][j] });
+        }
+      }
+    }
+  });
+  return drawnNumber;
+};
+
+const isWinner = (board: Board): boolean => {
+  // Horizontal
+  for (let i = 0; i < BOARD_DIMENSION; i++) {
+    if (
+      board.markedCoords
+        .map((coord: Coord) => coord.x)
+        .filter((xCoord: number) => xCoord === i).length === BOARD_DIMENSION
+    ) {
+      return true;
+    }
+  }
+  // Vertical
+  for (let i = 0; i < BOARD_DIMENSION; i++) {
+    if (
+      board.markedCoords
+        .map((coord: Coord) => coord.y)
+        .filter((yCoord: number) => yCoord === i).length === BOARD_DIMENSION
+    ) {
+      return true;
+    }
+  }
+  // Diagonal
+  let diagonalWin = true;
+  for (let i = 0; i < BOARD_DIMENSION; i++) {
+    if (
+      !board.markedCoords.some((coord: Coord) => coord.x === i && coord.y == i)
+    ) {
+      diagonalWin = false;
+    }
+  }
+  if (diagonalWin) {
+    return true;
+  }
+  // Reverse Diagonal
+  let reverseDiagonalWin = true;
+  for (let i = BOARD_DIMENSION; i >= 0; --i) {
+    if (
+      !board.markedCoords.some((coord: Coord) => coord.x === i && coord.y == i)
+    ) {
+      reverseDiagonalWin = false;
+    }
+  }
+  if (reverseDiagonalWin) {
+    return true;
+  }
+  return false;
+};
+
+const buildBoard = (lines: string[]): Board => {
+  const board = [] as number[][];
+  for (let i = 0; i < BOARD_DIMENSION; i++) {
+    const currentRow = lines[i].trim().replace(/  /g, " ").split(" ");
+    for (let j = 0; j < BOARD_DIMENSION; j++) {
+      if (!board[i]) board[i] = [];
+      board[i][j] = Number(currentRow[j]);
+    }
+  }
+  return { board, markedCoords: [] };
+};
+
+const buildBoards = (lines: string[]): Board[] => {
+  const boards: Board[] = [];
+  for (let i = 2; i < lines.length; i++) {
+    const currentBoardLines: string[] = [];
+    for (let j = 0; j < BOARD_DIMENSION; j++) {
+      currentBoardLines.push(lines[i + j]);
+    }
+    boards.push(buildBoard(currentBoardLines));
+    i += BOARD_DIMENSION;
+  }
+  return boards;
+};
 
 const buildBoardCollection = (filepath: string): BoardCollection => {
   const lines = readFile(filepath);
   const collection: BoardCollection = {
-    numbers: lines[0].split(",").map(Number),
-    boards: [], // TODO
+    numberCage: lines[0].split(",").map(Number),
+    boards: buildBoards(lines),
   };
   return collection;
 };
 
-const solve = (filepath: string): number => {
-  const collection = buildBoardCollection(filepath);
-  return 0;
-};
+const solve = (filepath: string): number =>
+  runGame(buildBoardCollection(filepath));
 
 export const runTest = (): number => solve("data/day4/test-data.txt");
 
--- src/day4/part2.ts
+++ src/day4/part2.ts
@@ -1,14 +1,161 @@
-// import { readFile, reportAnswer } from "../utils";
+import { readFile, reportAnswer } from "../utils";
 
-// const solve = (filepath: string): number => {
-//   const input = readFile(filepath);
-//   return 0;
-// };
+const BOARD_DIMENSION = 5;
 
-// export const runTest = (): number => solve("data/day4/test-data.txt");
+type BoardCollection = {
+  boards: Board[];
+  numberCage: number[];
+};
 
-// export const run = (): number => {
-//   const answer = solve("data/day4/data.txt");
-//   reportAnswer(4, 2, answer);
-//   return answer;
-// };
+type Coord = { x: number; y: number; value: number };
+
+type Board = {
+  board: number[][];
+  markedCoords: Coord[];
+  won: boolean;
+};
+
+const runGame = (collection: BoardCollection): number => {
+  let round = 0;
+  let winningBoard = {} as Board;
+  let mostRecentlyPulledNumber;
+  while (!collection.boards.every((board: Board) => board.won)) {
+    mostRecentlyPulledNumber = processRound(collection, round);
+    collection.boards.forEach((board: Board) => {
+      if (isWinner(board)) {
+        winningBoard = board;
+        board.won = true;
+        return;
+      }
+    });
+    round++;
+  }
+  return calculateScore(winningBoard, mostRecentlyPulledNumber as number);
+};
+
+const calculateScore = (
+  board: Board,
+  mostRecentlyPulledNumber: number
+): number => {
+  let sum = 0;
+  for (let i = 0; i < BOARD_DIMENSION; i++) {
+    for (let j = 0; j < BOARD_DIMENSION; j++) {
+      if (
+        !board.markedCoords.some(
+          (coord: Coord) => coord.x === i && coord.y === j
+        )
+      ) {
+        sum += board.board[i][j];
+      }
+    }
+  }
+  console.log(sum); // how is sum 1 higher than it should be?
+  return sum * mostRecentlyPulledNumber;
+};
+
+const processRound = (collection: BoardCollection, round: number): number => {
+  const drawnNumber = collection.numberCage[round];
+  collection.boards.forEach((board: Board) => {
+    for (let i = 0; i < BOARD_DIMENSION; i++) {
+      for (let j = 0; j < BOARD_DIMENSION; j++) {
+        if (board.board[i][j] === drawnNumber) {
+          board.markedCoords.push({ x: i, y: j, value: board.board[i][j] });
+        }
+      }
+    }
+  });
+  return drawnNumber;
+};
+
+const isWinner = (board: Board): boolean => {
+  // Horizontal
+  for (let i = 0; i < BOARD_DIMENSION; i++) {
+    if (
+      board.markedCoords
+        .map((coord: Coord) => coord.x)
+        .filter((xCoord: number) => xCoord === i).length === BOARD_DIMENSION
+    ) {
+      return true;
+    }
+  }
+  // Vertical
+  for (let i = 0; i < BOARD_DIMENSION; i++) {
+    if (
+      board.markedCoords
+        .map((coord: Coord) => coord.y)
+        .filter((yCoord: number) => yCoord === i).length === BOARD_DIMENSION
+    ) {
+      return true;
+    }
+  }
+  // Diagonal
+  let diagonalWin = true;
+  for (let i = 0; i < BOARD_DIMENSION; i++) {
+    if (
+      !board.markedCoords.some((coord: Coord) => coord.x === i && coord.y == i)
+    ) {
+      diagonalWin = false;
+    }
+  }
+  if (diagonalWin) {
+    return true;
+  }
+  // Reverse Diagonal
+  let reverseDiagonalWin = true;
+  for (let i = BOARD_DIMENSION; i >= 0; --i) {
+    if (
+      !board.markedCoords.some((coord: Coord) => coord.x === i && coord.y == i)
+    ) {
+      reverseDiagonalWin = false;
+    }
+  }
+  if (reverseDiagonalWin) {
+    return true;
+  }
+  return false;
+};
+
+const buildBoard = (lines: string[]): Board => {
+  const board = [] as number[][];
+  for (let i = 0; i < BOARD_DIMENSION; i++) {
+    const currentRow = lines[i].trim().replace(/  /g, " ").split(" ");
+    for (let j = 0; j < BOARD_DIMENSION; j++) {
+      if (!board[i]) board[i] = [];
+      board[i][j] = Number(currentRow[j]);
+    }
+  }
+  return { board, markedCoords: [], won: false };
+};
+
+const buildBoards = (lines: string[]): Board[] => {
+  const boards: Board[] = [];
+  for (let i = 2; i < lines.length; i++) {
+    const currentBoardLines: string[] = [];
+    for (let j = 0; j < BOARD_DIMENSION; j++) {
+      currentBoardLines.push(lines[i + j]);
+    }
+    boards.push(buildBoard(currentBoardLines));
+    i += BOARD_DIMENSION;
+  }
+  return boards;
+};
+
+const buildBoardCollection = (filepath: string): BoardCollection => {
+  const lines = readFile(filepath);
+  const collection: BoardCollection = {
+    numberCage: lines[0].split(",").map(Number),
+    boards: buildBoards(lines),
+  };
+  return collection;
+};
+
+const solve = (filepath: string): number =>
+  runGame(buildBoardCollection(filepath));
+
+export const runTest = (): number => solve("data/day4/test-data.txt");
+
+export const run = (): number => {
+  const answer = solve("data/day4/data.txt");
+  reportAnswer(4, 1, answer);
+  return answer;
+};
