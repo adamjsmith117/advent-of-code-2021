33b3d0d9f9490a26cbff6ec88e1d05c234d94e19
adamjsmith117 <adamjsmith117@gmail.com>
2022-02-12 21:28:19 -0700

    day 8 setup

---
+++ src/day8/part1.ts
@@ -0,0 +1,14 @@
+import { readFile, reportAnswer } from '../utils'
+
+const solve = (filepath: string): number => {
+  const input = readFile(filepath)
+  return 0
+}
+
+export const runTest = (): number => solve('data/day8/test-data.txt')
+
+export const run = (): number => {
+  const answer = solve('data/day8/data.txt')
+  reportAnswer(8, 1, answer)
+  return answer
+}
